7f5c246925c76576fb85e1ce32aaa47074a9360e
Jonathan Junxiao Zhu <jonathanzhu1982@hotmail.com>
2016-10-27 10:27:56 -0500

    Create renew_mysql.py

---
+++ database/renew_mysql.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 23 09:35:38 2016
+
+@author: Junxiao
+"""
+
+import os
+
+os.system("python .\\CreateTables\\cost_of_living_mysql.py")
+os.system("python .\\CreateTables\\crime_mysql.py")
+os.system("python .\\CreateTables\\demographics_mysql.py")
+os.system("python .\\CreateTables\\education_mysql.py")
+os.system("python .\\CreateTables\\employment_mysql.py")
+os.system("python .\\CreateTables\\housing_mysql.py")
+os.system("python .\\CreateTables\\weather_mysql.py")
